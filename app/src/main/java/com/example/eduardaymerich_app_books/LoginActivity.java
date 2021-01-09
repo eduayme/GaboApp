f14de1be439e1c6b808433a71be24423702fce10
Eduard Aymerich <eduardaymerich@gmail.com>
2021-01-10 00:26:17 +0100

    enh: get values login and register

--- app/src/main/java/com/example/eduardaymerich_app_books/LoginActivity.java
+++ app/src/main/java/com/example/eduardaymerich_app_books/LoginActivity.java
@@ -5,14 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class LoginActivity<textView> extends AppCompatActivity {
+    EditText username, password;
+    Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        username = findViewById(R.id.input_username);
+        password = findViewById(R.id.input_password);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String usernameValue = username.getText().toString();
+                String passwordValue = password.getText().toString();
+            }
+        });
     }
 
     public void onClickToRegister(View v)
--- app/src/main/java/com/example/eduardaymerich_app_books/RegisterActivity.java
+++ app/src/main/java/com/example/eduardaymerich_app_books/RegisterActivity.java
@@ -5,14 +5,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText email, username, password, confirm_password;
+    Button btn_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        email = findViewById(R.id.edit_email);
+        username = findViewById(R.id.edit_username);
+        password = findViewById(R.id.edit_password);
+        confirm_password = findViewById(R.id.edit_confirm_password);
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String emailValue = email.getText().toString();
+                String usernameValue = username.getText().toString();
+                String passwordValue = password.getText().toString();
+                String confirmValue = confirm_password.getText().toString();
+            }
+        });
     }
 
     public void onClickToLogin(View v)
