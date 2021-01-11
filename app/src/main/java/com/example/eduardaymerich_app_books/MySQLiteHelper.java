0e7556fc0235f284dc239eb945984796a121a9df
Eduard Aymerich <eduardaymerich@gmail.com>
2021-01-11 16:14:34 +0100

    add: insert sqlite

--- app/src/main/java/com/example/eduardaymerich_app_books/MySQLiteHelper.java
+++ app/src/main/java/com/example/eduardaymerich_app_books/MySQLiteHelper.java
@@ -1,25 +1,27 @@
 package com.example.eduardaymerich_app_books;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class MySQLiteHelper extends SQLiteOpenHelper {
 
-    public static final String TABLE_COMMENTS = "comments";
-    public static final String COLUMN_ID = "_id";
-    public static final String COLUMN_COMMENT = "comment";
-    private static final String DATABASE_NAME = "commments.db";
+    private static final String DATABASE_NAME = "gaboapp.db";
     private static final int DATABASE_VERSION = 1;
 
     // Database creation sql statement
-    private static final String DATABASE_CREATE = "create table "
-            + TABLE_COMMENTS + "(" + COLUMN_ID
-            + " integer primary key autoincrement, " + COLUMN_COMMENT
-            + " text not null);";
+    private static final String DATABASE_CREATE =
+            "CREATE TABLE if not exists `users` " +
+                    "( `id` INTEGER PRIMARY KEY AUTOINCREMENT, `username` TEXT," +
+                    " `password` TEXT, `email` TEXT )";
 
     public MySQLiteHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    
+    public void insertUser(ContentValues contentValues) {
+        getWritableDatabase().insert("user", "", contentValues );
     }
 
     @Override
