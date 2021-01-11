bff5056c795fa3d57835a8357cdf1a3661c62df8
Eduard Aymerich <eduardaymerich@gmail.com>
2021-01-11 23:07:57 +0100

    add: book adapter

--- app/src/main/java/com/example/eduardaymerich_app_books/adapters/BookAdapter.java
+++ app/src/main/java/com/example/eduardaymerich_app_books/adapters/BookAdapter.java
@@ -1,22 +1,53 @@
 package com.example.eduardaymerich_app_books.adapters;
 
 import android.content.Context;
+import android.net.Uri;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.eduardaymerich_app_books.R;
 import com.example.eduardaymerich_app_books.models.Book;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
 public class BookAdapter extends ArrayAdapter<Book> {
+    private static class ViewHolder {
+        public ImageView ivCover;
+        public TextView tvTitle;
+        public TextView tvAuthor;
+    }
+
     public BookAdapter(Context context, ArrayList<Book> aBooks) {
         super(context, 0, aBooks);
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        // TODO: Vista de cada libro
-        return null;
+        final Book book = getItem(position);
+        ViewHolder viewHolder;
+
+        if( convertView == null ) {
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.view_book, parent, false);
+            viewHolder.ivCover = (ImageView)convertView.findViewById(R.id.ivBookCover);
+            viewHolder.tvTitle = (TextView)convertView.findViewById(R.id.tvTitle);
+            viewHolder.tvAuthor = (TextView)convertView.findViewById(R.id.tvAuthor);
+            convertView.setTag(viewHolder);
+        }
+        else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        viewHolder.tvTitle.setText(book.getTitle());
+        viewHolder.tvAuthor.setText(book.getAuthor());
+        Picasso.with(getContext()).load(Uri.parse(book.getCoverUrl())).error(R.drawable.no_cover).into(viewHolder.ivCover);
+
+        return convertView;
     }
 }
