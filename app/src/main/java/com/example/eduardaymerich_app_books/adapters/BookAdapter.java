6b9c8645993de05055ca06e860d448a0c433f7bb
Eduard Aymerich <eduardaymerich@gmail.com>
2021-01-11 20:43:00 +0100

    add: book view

--- app/src/main/java/com/example/eduardaymerich_app_books/adapters/BookAdapter.java
+++ app/src/main/java/com/example/eduardaymerich_app_books/adapters/BookAdapter.java
@@ -1,4 +1,22 @@
 package com.example.eduardaymerich_app_books.adapters;
 
-public class BookAdapter {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import com.example.eduardaymerich_app_books.models.Book;
+
+import java.util.ArrayList;
+
+public class BookAdapter extends ArrayAdapter<Book> {
+    public BookAdapter(Context context, ArrayList<Book> aBooks) {
+        super(context, 0, aBooks);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // TODO: Vista de cada libro
+        return null;
+    }
 }
